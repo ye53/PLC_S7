90626c9f1a98fa2d0e21c4c065b80374f1fb6219
Zatangzitai <878219005@qq.com>
2023-05-18 20:01:29 +0800

    rpmmeasure

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,10 +4,13 @@
 #include <QDebug>
 #include "basechange.h"
 #include "login.h"
+#include "rpmmeasurewidget.h"
+#include <QHBoxLayout>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
     , m_threadPool(this)
+
 {
     ui->setupUi(this);
 
@@ -19,7 +22,6 @@ MainWindow::MainWindow(QWidget *parent)
     m_threadPool.setMaxThreadCount(4);
 
     setWindowIcon(QIcon(":/actions/stock_about.png"));
-
 
 
     sql->CreateTable();
@@ -308,6 +310,14 @@ void MainWindow::on_actionASCII_triggered()
     ui->show();
 }
 
+
+
+
+void MainWindow::on_action1_triggered()
+{
+    RpmMeasureWidget *ui = new RpmMeasureWidget();
+    ui->show();
+}
 
 void MainWindow::on_put_list_clicked()
 {
